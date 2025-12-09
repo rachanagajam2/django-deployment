a6397235816de3762380e8511b17c2140fd3d825
Rachanagajam2 <rachanagajam2@gmail.com>
2025-12-09 23:57:22 +0530

    onemorehome added

--- projdeploy/projdeploy/urls.py
+++ projdeploy/projdeploy/urls.py
@@ -20,5 +20,6 @@ from basic.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home)
+    path('', home, name='home'),
+    path('home/',home,name='home_page')
 ]
